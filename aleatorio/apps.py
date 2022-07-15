77c18d37da6ea741f95dc5934b2eccfe52e504e2
thompson-pilot <rthomques@gmail.com>
2022-07-15 12:50:16 -0300

    First files commit

---
+++ aleatorio/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class AleatorioConfig(AppConfig):
+    name = 'aleatorio'
---
+++ aleatorio/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+import random
+
+
+def index(request):
+    return render(request, "input.html")
+
+
+def randomnum(request):
+	num1 = request.POST['num1']
+	num2 = request.POST['num2']
+
+	if num1.isdigit() and num2.isdigit():
+		a = int(num1)
+		myGame = [n + 1 for n in range(a)]
+
+		b = int(num2)
+		res = random.sample(myGame, b)
+		res.sort()
+
+        #return render(request, "result.html", {"result":res})
+		return render(request, "result.html", {"result": (', '.join(str(e) for e in res))})
+
+	else:
+		res = "Only digits are allowed"
+		return render(request, "result.html", {"result": res})
